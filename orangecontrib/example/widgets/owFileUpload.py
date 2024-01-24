3d26f11c20deabf4b7dac44ae53947ef1846a886
Anamari-ctrl <ao6477@student.uni-lj.si>
2024-01-24 15:47:58 +0100

    code updates

--- orangecontrib/example/widgets/owFileUpload.py
+++ orangecontrib/example/widgets/owFileUpload.py
@@ -1,11 +1,19 @@
+import os
+
 import numpy as np
-from Orange.data import Table
-from orangewidget.widget import settings
-from Orange.widgets.settings import Setting
-from Orange.widgets.widget import OWWidget, Input, Output, Msg
-from AnyQt.QtWidgets import QMessageBox, QGridLayout
-from PyQt5.QtWidgets import QFileDialog, QPushButton
 from PIL import Image
+from AnyQt.QtWidgets import QFileDialog
+
+
+from orangewidget.utils.signals import summarize, PartialSummary
+from Orange.widgets.settings import Setting
+from Orange.widgets.widget import OWWidget, Output, Msg
+from Orange.widgets import gui
+
+
+@summarize.register
+def summarize_ndarray(a:np.ndarray):
+    return PartialSummary(f"{a.shape[0]}x{a.shape[1]}", f"Image of size {a.shape[0]}x{a.shape[1]}")
 
 class uploadFile(OWWidget):
     # Widget needs a name, or it is considered an abstract widget
@@ -17,23 +25,17 @@ class uploadFile(OWWidget):
     keywords = "data, load, read, open, image"
     category = "Example - documentation"
 
-    label = Setting("")
 
     #TODO: We declare output as numpy array
+
     class Outputs:
-        image = Output("image", np.ndarray, default=True, auto_summary=False)
+        image = Output("image", np.ndarray, default=True)
 
-    proportion = settings.Setting(50)
-    commitOnChange = settings.Setting(0)
+    filename = Setting(None)
 
-    #TODO: Why is important that this is false
     want_main_area = False
-    buttons_area_orientation = False
 
     # TODO: We declare classes for Warning, Information and Errors
-    class Information(OWWidget.Information):
-        no_file_selected = Msg("No file selected")
-        no_file_saved = Msg("No file saved")
 
     class Warning(OWWidget.Warning):
         file_too_big = Msg("File too big")
@@ -46,37 +48,34 @@ class uploadFile(OWWidget):
 
     def __init__(self):
         super().__init__()
-        self.image = None
 
-        layout = QGridLayout()
-        layout.setSpacing(4)
-
-        self.load_button = QPushButton('Load File', self)
-        self.load_button.clicked.connect(self.browse_file)
-        layout.addWidget(self.load_button, 0, 0)
+        gui.button(self.controlArea, self, label="Load image", callback=self.browse_file)
+        box = gui.widgetBox(self.controlArea, "Info")
+        self.label = gui.widgetLabel(box, "")
+        self.load_image()
 
     def browse_file(self):
-        self.image, _ = QFileDialog.getOpenFileName(
+        filename, _ = QFileDialog.getOpenFileName(
             self, 'Open File', '', 'Image Files (*.gif *.jpg *.jpeg *.png *.svg);;All Files (*)'
         )
-        # TODO:Preverjanje ce je datoteka izbrana
-        msg = QMessageBox()
-        msg.setWindowTitle("File Upload")
-        msg.setText(f"Do you want to upload the file? {self.image}")
-        btn1 = QMessageBox.Yes
-        btn2 = QMessageBox.No
-        msg.setStandardButtons(btn1 | btn2)
-        msg.setDefaultButton(btn1)
-        msg.buttonClicked.connect(self.popup_clicked)
-        x = msg.exec_()
+        if filename is None:
+            return
+
+        self.filename = filename
+        self.load_image()
+
+    def load_image(self):
+        if self.filename is None:
+            self.label.setText("No file selected")
+            img = None
+        else:
+            self.label.setText(self.filename)
+            img = np.array(Image.open(self.filename))
+
+        self.Outputs.image.send(img)
 
 
-    def popup_clicked(self, i):
-        if i.text() == ("&Yes"):
-            img = np.array(Image.open(self.image))
-            Image.fromarray(img).save('uploadedFile.jpg')
-            self.Outputs.image.send(img)
-            self.close()
+
 
 
 
