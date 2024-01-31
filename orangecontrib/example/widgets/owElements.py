e84880c44c90ac3d07f4ba956ff6820675c2e189
Anamari-ctrl <ao6477@student.uni-lj.si>
2024-01-31 14:18:29 +0100

    comboBox working

--- orangecontrib/example/widgets/owElements.py
+++ orangecontrib/example/widgets/owElements.py
@@ -2,6 +2,7 @@ from AnyQt.QtWidgets import QGridLayout
 from AnyQt.QtCore import Qt
 from Orange.widgets.widget import OWWidget
 from Orange.widgets import gui
+from orangewidget.settings import Setting
 
 
 class Elements(OWWidget):
@@ -11,29 +12,24 @@ class Elements(OWWidget):
 
     want_main_area = False
     buttons_area_orientation = None
+    resizing_enabled = False
+
+    color_channel = Setting(0)
 
     def __init__(self):
         super().__init__()
 
-        layout = QGridLayout()
-        layout.setSpacing(3)
-
-        gui.widgetBox(self.controlArea, orientation=layout, box='Color Channels')
-        vbox = gui.radioButtonsInBox(None, self, "color_channel", box=True,
-                                     callback=self.dropdown_changed, addToLayout=False)
-
-        rc_button = gui.appendRadioButton(vbox, "Channel R", addToLayout=False)
-        layout.addWidget(rc_button, 0, 0, Qt.AlignCenter)
-
-        gc_button = gui.appendRadioButton(vbox, "Channel G", addToLayout=False)
-        layout.addWidget(gc_button, 0, 1, Qt.AlignCenter)
-
-        bc_button = gui.appendRadioButton(vbox, "Channel B", addToLayout=False)
-        layout.addWidget(bc_button, 0, 2, Qt.AlignCenter)
+        gui.comboBox(self.controlArea, self, "color_channel", box='Color Channels', items=["R", "G", "B"],
+                     sendSelectedValue=True, callback=self.dropdown_changed)
 
     @staticmethod
     def dropdown_changed():
         print("Dropdown changed")
+
+
+
+
+
 
 
 if __name__ == "__main__":
