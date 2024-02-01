c8bf216eb446ccd9bb1b627187ae8e710b64dd0b
Anamari-ctrl <ao6477@student.uni-lj.si>
2024-02-01 09:36:56 +0100

    working on rgb combobox

--- orangecontrib/example/widgets/owElements.py
+++ orangecontrib/example/widgets/owElements.py
@@ -1,14 +1,30 @@
+import numpy as np
 from AnyQt.QtWidgets import QGridLayout
 from AnyQt.QtCore import Qt
-from Orange.widgets.widget import OWWidget
+from Orange.widgets.widget import OWWidget, Input, Output
 from Orange.widgets import gui
 from orangewidget.settings import Setting
+from enum import Enum
 
+class Color(Enum):
+    RED = 1
+    GREEN = 2
+    BLUE = 3
+
+class WidgetSettings:
+    def __init__(self):
+        self.color = Color.RED
 
 class Elements(OWWidget):
     name = "GUI Elements"
     description = "GUI Elements"
     icon = "icons/elements.png"
+
+    class Inputs:
+        image = Input("Image", np.ndarray)
+
+    class Outputs:
+        image = Output("Image", np.ndarray)
 
     want_main_area = False
     buttons_area_orientation = None
@@ -18,17 +34,27 @@ class Elements(OWWidget):
 
     def __init__(self):
         super().__init__()
+        self.settings = WidgetSettings()
 
-        gui.comboBox(self.controlArea, self, "color_channel", box='Color Channels', items=["R", "G", "B"],
-                     sendSelectedValue=True, callback=self.dropdown_changed)
+        self.rgb_combobox = gui.comboBox(self.controlArea, self, "color_channel", box='Color Channels',
+                                         items=["R", "G", "B"],
+                                         sendSelectedValue=True, callback=self.dropdown_changed)
 
-    @staticmethod
-    def dropdown_changed():
+    def dropdown_changed(self):
         print("Dropdown changed")
+        print(self.rgb_combobox.currentText())
 
+        if self.rgb_combobox.currentText() == "R":
+            self.settings.color = Color.RED
+        elif self.rgb_combobox.currentText() == "G":
+            self.settings.color = Color.GREEN
+        elif self.rgb_combobox.currentText() == "B":
+            self.settings.color = Color.BLUE
 
-
-
+    def process_image(self):
+        # vzamem input
+        return 0
+        # self.settings.color -> tuki je ze izbrana barva
 
 
 
