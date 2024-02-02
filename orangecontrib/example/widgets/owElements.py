0e1c4ab87157b66f159afbdbaaaf0e6f97543f58
Anamari-ctrl <ao6477@student.uni-lj.si>
2024-02-02 13:28:54 +0100

    added checkboxes

--- orangecontrib/example/widgets/owElements.py
+++ orangecontrib/example/widgets/owElements.py
@@ -1,23 +1,7 @@
 import numpy as np
-from AnyQt.QtWidgets import QGridLayout
-from AnyQt.QtCore import Qt
 from Orange.widgets.widget import OWWidget, Input, Output
 from Orange.widgets import gui
 from orangewidget.settings import Setting
-from enum import Enum
-
-from orangewidget.utils.signals import summarize, PartialSummary
-
-
-@summarize.register
-def summarize_ndarray(a: np.ndarray):
-    return PartialSummary(f"{a.shape[0]}x{a.shape[1]}", f"Image of size {a.shape[0]}x{a.shape[1]}")
-
-
-class Color(Enum):
-    RED = 0
-    GREEN = 1
-    BLUE = 2
 
 
 class Elements(OWWidget):
@@ -38,21 +22,42 @@ class Elements(OWWidget):
     color_channel = Setting(0)
     invert = Setting(0)
     brightness = Setting(0)
+    contrast = Setting(0)
+    binarize = Setting(0)
 
     def __init__(self):
         super().__init__()
         self.image_array = None
 
-        self.rgb_combobox = gui.comboBox(self.controlArea, self, "color_channel", box='Color Channels',
-                                         items=["R", "G", "B"], callback=self.commit)
+        gui.comboBox(self.controlArea, self, "color_channel", box='Color Channels',
+                     items=["None", "R", "G", "B"], callback=self.commit)
 
-        self.check_box = gui.checkBox(self.controlArea, self, "invert",
-                                      'Invert pixels',
-                                      tooltip='Invert pixels',
-                                      callback=self.commit)
+        gui.checkBox(self.controlArea, self, "invert",
+                     'Invert pixels',
+                     tooltip='Invert pixels',
+                     callback=self.commit)
 
-        self.slider = gui.hSlider(self.controlArea, self, "brightness", box='Brightness',
-                                  callback=self.commit, minValue=0, maxValue=255, step=1)
+        gui.hSlider(self.controlArea, self, "brightness", box='Brightness',
+                    callback=self.commit, minValue=-100, maxValue=100, step=1)
+        # gui.hSlider(self.controlArea, self, "contrast", box='Contrast',
+        # callback=self.commit, minValue=-100, maxValue=100, step=1)
+        gui.checkBox(self.controlArea, self, "contrast",
+                     'Contrast',
+                     tooltip='contrast',
+                     callback=self.commit)
+
+        gui.checkBox(self.controlArea, self, "binarize",
+                     'Binarize',
+                     tooltip='binarize',
+                     callback=self.commit)
+
+        gui.spin(self.controlArea, self, "brightness", -100, 100, 1, label="Brightness",
+                 callback=self.commit)
+
+        display = gui.widgetBox(self.controlArea, "Display")
+        gui.checkBox(display, self, "binarize", "Binarize", callback=self.commit)
+        gui.checkBox(display, self, "invert", "Invert", callback=self.commit)
+
 
     @Inputs.image
     def set_image1(self, image):
@@ -62,20 +67,32 @@ class Elements(OWWidget):
         self.commit()
 
     def commit(self):
-        # color = Color(self.rgb_combobox.currentIndex())
-        # print(f"color {self.color_channel}")
-        print()
-
         temp = self.image_array.copy()
-        if self.color_channel == Color.RED.value:
+
+        if self.color_channel == 1:
             temp[:, :, (1, 2)] = 0
-        elif self.color_channel == Color.GREEN.value:
+        elif self.color_channel == 2:
             temp[:, :, (0, 2)] = 0
-        elif self.color_channel == Color.BLUE.value:
+        elif self.color_channel == 3:
             temp[:, :, (0, 1)] = 0
 
         if self.invert:
             temp = 255 - temp
+
+        if self.brightness:
+            if self.brightness < 0:
+                temp = temp - self.brightness
+                temp[temp < 0] = 0
+            else:
+                temp = temp + self.brightness
+                temp[temp > 255] = 255
+
+        if self.contrast:
+            temp = temp * 1.5
+            temp[temp > 255] = 255
+
+        if self.binarize:
+            temp = (temp > 127) * 255
 
         self.Outputs.image.send(temp)
 
